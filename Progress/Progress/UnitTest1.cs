7d78bcd0e295de3530edadd5cf653be70dba393e
Terje Sandstrom <terje.sandstrom.personal@outlook.com>
2019-07-10 19:07:57 +0200

    Added code

---
+++ Progress/Progress/UnitTest1.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void ShowProgress()
+        {
+            TestContext.Progress.WriteLine("Progress");
+        }
+
+        [Test]
+        public void ShowOut()
+        {
+            TestContext.Out.WriteLine("Out");
+        }
+
+        [Test]
+        public void ShowWriteLine()
+        {
+            TestContext.WriteLine("WriteLine");
+        }
+        [Test]
+        public void ShowWriteLineFails()
+        {
+            TestContext.WriteLine("WriteLine");
+            Assert.Fail("This failed");
+        }
+
+    }
+}
